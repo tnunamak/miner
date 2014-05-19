a631f010ad47d82a949fd22115afffad7b087e30
Tim Nunamaker <tim.nunamaker@bazaarvoice.com>
2014-05-19 10:46:08 -0500

    Refactor input handling and purchase logic

--- src/miner/core.clj
+++ src/miner/core.clj
@@ -45,11 +45,11 @@
                                             :silver 0
                                             :ruby 0}))
 
-(defn empty-at-cursor [game]
+(defn update-at-cursor [game mineral-type]
   (assoc-in
    game
    [:board (get-cursor-coords game)]
-   :empty))
+   mineral-type))
 
 (defn count-mineral [game mineral]
   (if (< (count-cargo game) (:cargo-capacity game))
@@ -62,35 +62,106 @@
     (cond
      (fuel? mineral) (assoc-in game [:fuel] (:fuel-capacity game))
      (shop? mineral) (sell-cargo game)
-     (valuable? mineral) (empty-at-cursor (count-mineral game mineral))
-     :else (empty-at-cursor game))))
+     (valuable? mineral) (update-at-cursor (count-mineral game mineral) :empty)
+     :else (update-at-cursor game :empty))))
+
+(defn contains-value? [aSeq aVal]
+  (reduce #(or %1 (= %2 aVal)) false aSeq))
+
+(defn have-enough-money-for [game purchase]
+  (>= (:money game) (purchase (:prices game))))
+
+(defn get-new-origin [[x y] move-direction]
+  (case move-direction
+    :left [(- x 1) y]
+    :right [(+ x 1) y]
+    :up [x (- y 1)]
+    :down [x (+ y 1)]
+    [x y]))
+
+(defn get-mineral-type
+  ([game]
+   (get-mineral-type game (:origin game)))
+  ([game origin]
+   (get (:board game) (get-cursor-coords (:screen game) origin))))
+
+(defn assoc-map-vals [aMap updates]
+  (reduce (fn [aMap [aKey aVal]] ;; There has to be a better way to update multiple values in a map
+            (assoc-in aMap [aKey] aVal))
+          aMap
+          updates))
+
+(defn try-buy [game key-pressed]
+  (if (:buy-active game)
+    (let [desired-purchase (case key-pressed
+                             \c :cargo-capacity
+                             \f :fuel-capacity
+                             \u :fuel
+                             \s :shop
+                             :none)
+          buy-fuel  (and (= desired-purchase :fuel)
+                         (and (= (get-mineral-type game) :empty)
+                              (have-enough-money-for game :fuel)))
+          buy-shop  (and (= desired-purchase :shop)
+                         (have-enough-money-for game :shop))
+          buy-fuel-capacity (and (= desired-purchase :fuel-capacity)
+                                 (have-enough-money-for game :fuel-capacity))
+          buy-cargo-capacity (and (= desired-purchase :cargo-capacity)
+                                  (have-enough-money-for game :cargo-capacity))
+          buy-something (or buy-fuel buy-shop buy-fuel-capacity buy-cargo-capacity)]
+      (assoc-map-vals game
+                      [
+                       [:fuel (:fuel game)] ;; todo get rid of the need for a default
+
+                       (if buy-something
+                         [:money (- (:money game) (desired-purchase (:prices game)))])
+                       (if buy-something
+                         [:buy-active false])
+                       (if buy-fuel
+                         [:board (:board (update-at-cursor game :fuel))])
+
+                       (if buy-shop
+                         [:board (:board (update-at-cursor game :shop))])
+
+                       (if buy-fuel-capacity
+                         [:fuel-capacity (* 1.5 (:fuel-capacity game))])
+                       (if buy-fuel-capacity
+                         [:fuel (int (math/ceil (* 1.5 (:fuel-capacity game))))])
+
+                       (if buy-cargo-capacity
+                         [:cargo-capacity (int (math/ceil (* 1.5 (:cargo-capacity game))))])
+                       ]))
+
+    game))
+
+(defn try-toggles [game key-pressed]
+  (assoc-map-vals game
+                  [(case key-pressed
+                     \b [:buy-active (not (:buy-active game))]
+                     \d [:drill-active (not (:drill-active game))]
+                     [:buy-active (true? (:buy-active game))])]))
+
+(defn try-move [game key-pressed]
+  (let [desired-origin (get-new-origin (:origin game) key-pressed)
+        mineral-type (get-mineral-type game desired-origin)
+        is-move (and (not= desired-origin (:origin game))
+                     (or (:drill-active game)
+                         (= mineral-type :empty)
+                         (= mineral-type :fuel)
+                         (= mineral-type :shop)))
+        new-fuel (if is-move (- (:fuel game) (if (:drill-active game) 3 1)) (:fuel game))
+        new-origin (if is-move desired-origin (:origin game))]
+    (assoc-map-vals game [[:origin new-origin]
+                          [:fuel new-fuel]])))
 
 (defn process-input [game]
   (let [[x y] (:origin game)
         key-pressed (s/get-key (:screen game))]
-    (reduce (fn [game [key-in-game value]] ;; There has to be a better way to update multiple values in a map
-              (assoc-in game [key-in-game] value))
-            game
-            (let [new-origin (case key-pressed
-                               :left [(- x 1) y]
-                               :right [(+ x 1) y]
-                               :up [x (- y 1)]
-                               :down [x (+ y 1)]
-                               [x y])
-                  mineral-type (get (:board game) (get-cursor-coords (:screen game) new-origin))
-                  is-move (and (not= new-origin [x y])
-                               (or (:drill-active game)
-                                   (= mineral-type :empty)
-                                   (= mineral-type :fuel)
-                                   (= mineral-type :shop)))]
-              [(if is-move
-                 [:fuel (- (:fuel game) (if (:drill-active game) 3 1))])
-               (if is-move
-                 [:origin new-origin]
-                 [:origin [x y]])
-               (case key-pressed
-                 \d [:drill-active (not (:drill-active game))]
-                 [:drill-active (:drill-active game)])]))))
+     (try-toggles
+      (try-buy
+      (try-move game key-pressed)
+      key-pressed)
+     key-pressed)))
 
 (defn get-magnitude [vect]
   (math/sqrt (apply + (map #(math/expt % 2) vect))))
@@ -145,11 +216,14 @@
 
 (defn draw-hud [game]
   (s/put-string (:screen game) 0 0 (str
-                                    "Cargo: " "(" (count-cargo game) "/" (:cargo-capacity game) ")" (:cargo game) "\t"
-                                    "Drill (d): " (if (:drill-active game) "on" "off") "\t"
-                                    "Fuel: " (:fuel game) "\t"
-                                    "$" (:money game)
-                                    "\t\t\t\t\t")))
+                                    "Drill (d): " (if (:drill-active game) "on" "off") " | "
+                                    "Fuel: " (:fuel game) " | "
+                                    "$" (:money game) " | "
+                                    "Buy (b)" (:buy-active game) " | "
+                                    "Cargo: " "(" (count-cargo game) "/" (:cargo-capacity game) ")" (:cargo game)
+                                    "\t\t\t\t\t"))
+  (if (:buy-active game)
+    (s/put-string (:screen game) 0 1 (str "Buy [(f)uel capacity: " (:fuel-capacity (:prices game)) "] [(c)argo capacity: " (:cargo-capacity (:prices game)) "] [f(u)el station: " (:fuel (:prices game)) "] [(s)hop: " (:shop (:prices game)) "]\t\t\t\t"))))
 
 (defn tick [my-pool game]
   (let [new-game (build-board (collect-mineral (process-input game)))]
@@ -172,13 +246,17 @@
 
   (tick my-pool {:screen scr
                  :board {}
+                 :prices {:fuel 500
+                          :shop 500
+                          :fuel-capacity 100
+                          :cargo-capacity 100}
                  :origin [0 0]
                  :cargo {:platinum 0
                          :gold 0
                          :silver 0
                          :ruby 0}
                  :cargo-capacity 10
-                 :money 0
+                 :money 1000
                  :fuel 100
                  :fuel-capacity 100}))
 
